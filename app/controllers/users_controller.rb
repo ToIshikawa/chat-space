c5fadff8f10a12085083b8c9151ee283c864b61e
ToIshikawa <welovegabba2.happycore@gmail.com>
2019-06-11 13:14:02 +0900

    incremental-search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
@@ -7,6 +8,14 @@ class UsersController < ApplicationController
       redirect_to root_path
     else
       render :edit
+    end
+  end
+
+  def index
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").where.not(id: current_user.id)
+    respond_to do |format|
+      format.html 
+      format.json
     end
   end
 
